be40549a1fb201c8cf7793b1bcf5b8fe7a0e0125
rakesh <tripathy10138@gmail.com>
2026-05-24 23:32:30 +0530

    cookie-parser

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -30,3 +30,6 @@ class ApiError extends Error {
 }
 
 export { ApiError }
+
+
+//this is a custom error class that extends the built-in Error class in JavaScript. It is designed to represent API errors and includes additional properties such as statusCode, data, message, success, and errors. The constructor takes in these parameters and initializes the properties accordingly. The stack trace is captured using Error.captureStackTrace if a stack is not provided.
